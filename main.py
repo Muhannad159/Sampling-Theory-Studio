aa68ea3f966eb77e9a9a7a595fcb6b8ee8b9b723
AliBadran716 <102072821+AliBadran716@users.noreply.github.com>
2023-10-20 23:35:52 +0300

    python files linked

---
+++ main.py
@@ -0,0 +1,29 @@
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.uic import loadUiType
+
+import os
+import sys
+from os import path
+
+FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "main.ui"))  # connects the Ui file with the Python file
+
+
+class MainApp(QMainWindow, FORM_CLASS):  # go to the main window in the form_class file
+
+    def __init__(self, parent=None):  # constructor to initiate the main window  in the design
+        super(MainApp, self).__init__(parent)
+        self.setupUi(self)
+
+
+
+def main():  # method to start app
+        app = QApplication(sys.argv)
+        window = MainApp()
+        window.show()
+        app.exec_()  # infinte Loop
+
+if __name__ == '__main__':
+     main()
