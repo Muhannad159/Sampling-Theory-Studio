ef12bcbd9e35d6ede19d359386de0bf72843d50b
ahmedalii3 <110257687+ahmedalii3@users.noreply.github.com>
2023-10-21 17:01:04 +0300

    بسم الله

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.uic import loadUiType
+from PyQt5.QtWidgets import QApplication, QFileDialog
 
+import pandas as pd
+import pyqtgraph as pg
 import os
 import sys
 from os import path
@@ -16,6 +19,70 @@ class MainApp(QMainWindow, FORM_CLASS):  # go to the main window in the form_cla
     def __init__(self, parent=None):  # constructor to initiate the main window  in the design
         super(MainApp, self).__init__(parent)
         self.setupUi(self)
+        self.graphicsView.setBackground('w')
+        self.graphicsView_2.setBackground('w')
+        self.graphicsView_3.setBackground('w')
+        self.signals_data = {}  # Define signals_data as a class attribute
+        self.count_signals = 0
+        self.file_names= []
+        self.handle_btn()
+    
+    def handle_btn(self):
+        self.actionOpen_file.triggered.connect(self.add_signal)
+    
+    
+    
+    
+    def plot_graph(self , ):
+        #self.color_detect(self.signals_data)
+        #self.graphicsView_1.setObjectName("graphicsView_1")
+        self.data_lines = []
+        
+        #self.graphicsView_1.setXRange(0, 616 * self.first_element_of_time)
+        for value in self.signals_data.values():
+            
+           pen = pg.mkPen(color=(255 , 0 , 0))
+           x = value[0]
+           y = value[1]
+           data_line = self.graphicsView.plot(x, y, pen=pen)
+           data_line.x_data = x
+           data_line.y_data = y
+           self.data_lines.append(data_line)
+        if not self.signals_data:
+            return
+        self.end_1 = 616 * self.first_element_of_time # Sets the end of the x range
+
+        #self.max_range_1()
+        #self.graphicsView_1.setYRange(-self.max_y_1, self.max_y_1)
+        
+    
+    def add_signal(self):
+         options  = QFileDialog().options()
+         options |= QFileDialog.ReadOnly
+         file_path, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv);;All Files (*)",
+                                                   options=options)
+         if file_path:
+            self.count_signals += 1
+            file_name = file_path.split("/")[-1]
+            self.file_names.append(file_name)
+            signal_data = pd.read_csv(file_name)
+            time_column = signal_data.iloc[:, 0]
+            values_column = signal_data.iloc[:, 1]
+            time_values = time_column.tolist()
+            v_values = values_column.tolist()
+            self.first_element_of_time = time_values[1]-time_values[0] 
+            self.max_x_1 = max(time_values)
+            self.number_of_points = len(time_values)
+                
+            # Making a new item in the dictionary, the new signal is given a key, and the values is given according to its data
+            self.signals_data[self.count_signals] = [time_values, v_values, 'Red', f"{'Signal'} - {self.count_signals}", file_name]
+            self.comboBox_2.addItem(f"{'Signal'} - {self.count_signals}")
+            self.start_flag_1 = False
+         self.plot_graph()    
+        
+        # Update the table with the latest data
+        
+
 
 
 
