8d5c78df0356d739c2d40ab9cde6f7ca797c1924
Muhannad Abdallah <Muhannadd159@gmail.com>
2023-10-23 23:19:37 +0300

    Calculate the max frequency and Sample the sinusoidals

--- mixer.py
+++ mixer.py
@@ -7,6 +7,7 @@ from UliEngineering.SignalProcessing.Simulation import sine_wave
 import os
 import sys
 from os import path
+import numpy.fft as fft
 import numpy as np
 import pyqtgraph as pg
 
@@ -45,6 +46,66 @@ class MixerApp(QDialog, FORM_CLASS):  # go to the main window in the form_class 
             self.syntheticSignal += sinusoidal
         self.sin_graphics_view.clear()
         self.sin_graphics_view.plot(self.sin_time, self.syntheticSignal, pen=pg.mkPen(color=(255, 0, 0)))
+        self.sample_rate = len(self.sin_time) 
+        self.max_freqs = self.max_frequencies = self.calculate_max_frequencies(self.sinusoidals, self.sample_rate)
+        self.overall_max_frequency = max(self.max_freqs)
+
+        self.sampled_signal = self.sample_signal(self.syntheticSignal, self.overall_max_frequency, self.sample_rate)
+        if self.sampled_signal is not None:
+        # Plot the sampled points as blue dots
+            self.sin_graphics_view.plot(self.sin_time[::len(self.sin_time) // len(self.sampled_signal)],
+                                    self.sampled_signal, pen=None, symbol='o', symbolPen=None, symbolBrush=(0, 0, 255))
+
+
+
+
+    def calculate_max_frequency(self, synthetic_signal, sample_rate):
+    # Step 1: Compute the Fourier Transform of the synthetic signal.
+        fft_result = fft.fft(synthetic_signal)
+
+    # Step 2: Find the frequency components in the FFT result.
+        frequency_values = fft.fftfreq(len(fft_result), 1 / sample_rate)
+
+    # Step 3: Identify the maximum frequency component.
+        magnitudes = np.abs(fft_result)
+        max_magnitude = np.max(magnitudes)
+        max_frequency_index = np.argmax(magnitudes)
+        max_frequency = frequency_values[max_frequency_index]
+
+    # Step 4: Return the maximum frequency.
+        return max_frequency
+
+    def calculate_max_frequencies(self, sinusoidals, sample_rate):
+        max_frequencies = []
+        for sinusoidal in sinusoidals:
+            max_freq = self.calculate_max_frequency(sinusoidal, sample_rate)
+            max_frequencies.append(max_freq)
+        return max_frequencies
+
+        #print(F_max)
+        # print(F_max)
+        # self.fmax = int(F_max)
+        # #print(self.fmax)
+        # self.fs = 5 * self.fmax
+        # #self.time_to_sample = 1/(self.fs)
+        # self.sampled_time = (time)[::int(100 / self.fs)]
+        # self.sampling_signal = (amplitude)[::int(100 / (self.fs))]
+        # # self.time_to_sample=1/(self.sampling_signal)
+        # self.sin_graphics_view.plot(
+        #     self.sampled_time, self.sampling_signal, pen=None, symbol='o')
+
+    def sample_signal(self, signal, max_frequency, sample_rate):
+    # Calculate the Nyquist frequency based on the max frequency
+        nyquist_frequency = max_frequency * 2
+
+    # Calculate the number of samples per period based on the Nyquist frequency
+        samples_per_period = int(sample_rate / nyquist_frequency)
+
+    # Use NumPy to downsample the signal to the appropriate number of samples
+        sampled_signal = signal[::samples_per_period]
+
+        return sampled_signal
+
        
 
 
