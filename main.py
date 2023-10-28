24382f6a65957e52be0d479d2b214ba91254ef2b
Muhannad Abdallah <Muhannadd159@gmail.com>
2023-10-28 19:49:26 +0200

    Refactoring

--- main.py
+++ main.py
@@ -3,59 +3,88 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.uic import loadUiType
 from PyQt5.QtWidgets import QApplication, QFileDialog
-
 import pandas as pd
 import pyqtgraph as pg
 import random  # Import the random module to add noise
-import os
 import sys
 import numpy as np
 from os import path
-import scipy.signal
-from scipy.interpolate import interp1d
-from scipy.signal import find_peaks
 
-FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "main.ui"))  # connects the Ui file with the Python file
+# Load the UI file and connect it with the Python file
+FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "main.ui"))
 
+class MainApp(QMainWindow, FORM_CLASS):
+    def __init__(self, parent=None):
+        """
+        Constructor to initiate the main window in the design.
 
-class MainApp(QMainWindow, FORM_CLASS):  # go to the main window in the form_class file
-
-    def __init__(self, parent=None):  # constructor to initiate the main window  in the design
+        Parameters:
+        - parent: The parent widget, which is typically None for the main window.
+        """
         super(MainApp, self).__init__(parent)
         self.setupUi(self)
+
+        # Configure graph view settings
         self.graphicsView.setBackground('w')
         self.graphicsView_2.setBackground('w')
         self.graphicsView_3.setBackground('w')
         self.graphicsView.setLabel('bottom', 'time')
         self.graphicsView_2.setLabel('bottom', 'time')
         self.graphicsView_3.setLabel('bottom', 'time')
-        self.signals_data = {}  # Define signals_data as a class attribute
-        self.count_signals = 0
-        self.noise_slider.setRange(0, 100)  # Set the range of the noise level as per your requirements
+
+        # Initialize data and settings
+        self.signals_data = {}  # Dictionary to store signal data
+        self.count_signals = 0  # Counter for the number of signals added
+        self.noise_slider.setRange(0, 100)  # Set the noise level range
         self.noise_level = 0  # Initialize the noise level
-        self.way_of_plotting_with_add = True
-        self.handle_btn()
+        self.freq_slider.setSingleStep(125)
+        self.error_threshold = 0.1  # Define your error threshold value
+        self.way_of_plotting_with_add = True  # Flag to determine the signal source
+        self.handle_btn()  # Connect UI elements to their handling functions
+
+    def center_on_screen(self):
+        """
+        Center the application window on the screen.
+        """
+        # Calculate the center coordinates for a 1920x1080 screen
+        screen_width = 1920
+        screen_height = 1080
+        window_width = self.frameGeometry().width()
+        window_height = self.frameGeometry().height()
+        x = (screen_width - window_width) // 2
+        y = (screen_height - window_height) // 2
+        # Set the window's position to the center
+        self.move(x, y)
 
     def handle_btn(self):
+        """
+        Connect UI elements to their respective handling functions.
+        """
         self.actionOpen_file.triggered.connect(self.add_signal)
         self.mix_signal_btn.triggered.connect(self.open_mixer)
         self.noise_slider.valueChanged.connect(self.update_noise_level)
-        self.delete_btn.clicked.connect(self.delete_signal)  # Connect the delete button here
+        self.delete_btn.clicked.connect(self.delete_signal)
         self.freq_slider.valueChanged.connect(self.update_fs)
 
     def open_mixer(self):
+        """
+        Open the signal mixer window and connect it with the main application.
+        """
         if self.signals_data:
             self.delete_signal()
         self.way_of_plotting_with_add = False
-        from mixer import MixerApp
-        self.mixer = MixerApp()
-        self.mixer.set_myapp(self)
-        self.mixer.show()
+        from mixer import MixerApp  # Import the signal mixer module
+        self.mixer = MixerApp()  # Create an instance of the MixerApp class
+        self.mixer.set_myapp(self)  # Set a reference to the main application
+        self.mixer.show()  # Display the mixer window
 
     def add_signal(self):
+         """
+         Load a CSV signal file, add it to the application's data, and plot it.
+         """
          options  = QFileDialog().options()
          options |= QFileDialog.ReadOnly
-         file_path, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv);;All Files (*)",
+         file_path, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (.csv);;All Files ()",
                                                    options=options)
          if file_path:
             if self.signals_data or not self.way_of_plotting_with_add:
@@ -76,14 +105,16 @@ class MainApp(QMainWindow, FORM_CLASS):  # go to the main window in the form_cla
             self.comboBox_2.addItem(f"{'Signal'} - {self.count_signals}")
             self.way_of_plotting_with_add = True
          self.fs = 125
-         self.freq_slider.setRange(int(self.fs / 2), int(self.fs * 3 / 2))
+         self.freq_slider.setRange(int(self.fs / 4), int(self.fs * 4))
          self.freq_slider.setSliderPosition(125)
          self.freq_slider.setValue(self.fs)  # Set the value to 125
          self.plot_graph()
 
-
     def plot_graph(self):
-        self.graphicsView.clear()
+        """
+        Plot the signals and related graphs.
+        """
+        self.clear_all()
         if self.way_of_plotting_with_add:
             for value in self.signals_data.values():
                 pen = pg.mkPen(color=(255, 0, 0))
@@ -103,11 +134,10 @@ class MainApp(QMainWindow, FORM_CLASS):  # go to the main window in the form_cla
                 self.graphicsView_2.plot(x, reconstructed_signal, pen=reconstruction_pen)
 
 
-                error_threshold = 0.1  # Define your error threshold valu
                 error = [abs(original - reconstructed) for original, reconstructed in
                          zip(noisy_y, reconstructed_signal)]
                 # Filter error values above the threshold
-                error_above_threshold = [err if err > error_threshold else 0 for err in error]
+                error_above_threshold = [err if err > self.error_threshold else 0 for err in error]
                 # Plot the error above the threshold
                 error_pen = pg.mkPen(color="r")
                 self.graphicsView_3.plot(x, error_above_threshold, pen=error_pen)
@@ -118,24 +148,35 @@ class MainApp(QMainWindow, FORM_CLASS):  # go to the main window in the form_cla
             self.graphicsView.plot(self.mixer.sin_time, noisy_y, pen=pg.mkPen(color=(255, 0, 0)))
             sampled_x, sampled_y = self.sample_signal(self.mixer.sin_time, noisy_y, self.fs, 0.005)
             self.graphicsView.plot(sampled_x, sampled_y, pen=None, symbol='o', symbolBrush='b')
-            self.graphicsView_2.plot(sampled_x, sampled_y, pen=None, symbol='o', symbolBrush='b')
             reconstructed_signal = self.sinc_interpolation(sampled_x, sampled_y, self.fs, self.mixer.sin_time)
             self.graphicsView_2.plot(self.mixer.sin_time, reconstructed_signal, pen=pg.mkPen(color=(255, 0, 0)))
-            error_threshold = 0.1  # Define your error threshold valu
+
             error = [abs(original - reconstructed) for original, reconstructed in
                      zip(noisy_y, reconstructed_signal)]
             # Filter error values above the threshold
-            error_above_threshold = [err if err > error_threshold else 0 for err in error]
+            error_above_threshold = [err if err > self.error_threshold else 0 for err in error]
             # Plot the error above the threshold
             error_pen = pg.mkPen(color="r")
             self.graphicsView_3.plot(self.mixer.sin_time, error_above_threshold, pen=error_pen)
-            self.graphicsView_3.setYRange(-6, 6)
            
 
     
-    def sample_signal(self, original_x, original_y, f_sample , start):
+    def sample_signal(self, original_x, original_y, f_sample, start):
+        """
+        Sample a signal at specified time intervals.
+
+        Args:
+            original_x (array-like): The time (or position) values of the original signal.
+            original_y (array-like): The corresponding signal values.
+            f_sample (float): The sampling frequency (or sampling rate).
+            start (float): The starting time for sampling.
+
+        Returns:
+            Tuple: A tuple containing two arrays - the new sample times and the sampled signal values.
+        """
         # Calculate the time interval between samples
         time_interval = 1 / f_sample
+
         # Create a new array of sample times based on the time interval
         new_sample_times = np.arange(start, max(original_x), time_interval)
 
@@ -150,25 +191,74 @@ class MainApp(QMainWindow, FORM_CLASS):  # go to the main window in the form_cla
 
         return new_sample_times, sampled_signal
 
+    def sinc_interpolation(self, sampled_x, sampled_y, f_sample, new_time_points):
+        """
+        Reconstruct a signal using sinc interpolation.
+
+        Args:
+            sampled_x (array-like): The time (or position) values at which the signal is sampled.
+            sampled_y (array-like): The corresponding sampled signal values.
+            f_sample (float): The sampling frequency (or sampling rate).
+            new_time_points (array-like): The time points at which you want to reconstruct the signal.
+
+        Returns:
+            np.array: The reconstructed signal values at new_time_points.
+        """
+        time_interval = 1 / f_sample
+        reconstructed_signal = np.zeros(len(new_time_points))
+
+        for n, sample in enumerate(sampled_y):
+            reconstructed_signal += sample * np.sinc((new_time_points - sampled_x[n]) / time_interval)
+
+        return reconstructed_signal
 
     def clear_all(self):
+        """
+        Clear all three graphics views used for plotting signals.
+        """
         self.graphicsView.clear()
         self.graphicsView_2.clear()
         self.graphicsView_3.clear()
 
     def update_noise_level(self, value):
+        """
+        Update the noise level and re-plot the signals with the new noise level.
+
+        Args:
+            value (int): The noise level value.
+        """
         self.clear_all()
+
+        # Adjust the noise level based on the plotting mode
         if self.way_of_plotting_with_add:
             self.noise_level = value / 100
         else:
             self.noise_level = value / 10
+
+        # Update the noise level label in the UI
         self.nsr_lbl.setText(f"Noise to Signal Ratio {str(self.noise_level)}")
+
+        # Re-plot the signals with the updated noise level
         self.plot_graph()
 
     def update_fs(self, value):
+        """
+        Update the sampling frequency and re-plot the signals with the new frequency.
+
+        Args:
+            value (int): The new sampling frequency.
+        """
         self.fs = value
+        self.freq_slider.setSingleStep(125)  # Set step size
         self.freq_lbl.setText(f"Sampling frequency is {str(self.fs)}")
-        self.clear_all()
+        if self.way_of_plotting_with_add:
+            if self.fs > 125:
+                self.error_threshold = 0.5  # Define your error threshold value
+        else:
+            print(self.fs)
+            print(self.mixer.overall_max_frequency * 2)
+            if self.fs >= self.mixer.overall_max_frequency * 2:
+                self.error_threshold = 4  # Define your error threshold value
         self.plot_graph()
 
         # Update to set the slider position and value to 125 if value is 125
@@ -176,15 +266,29 @@ class MainApp(QMainWindow, FORM_CLASS):  # go to the main window in the form_cla
             self.freq_slider.setSliderPosition(125)
             self.freq_slider.setValue(125)
 
-
     def reindex_dict_keys(self, dictionary):
+        """
+        Reindex dictionary keys to start from 1.
+
+        Args:
+            dictionary (dict): The input dictionary.
+
+        Returns:
+            dict: A dictionary with reindexed keys.
+        """
         return {i: value for i, (key, value) in enumerate(dictionary.items(), start=1)}
 
-        # A function to refill the comboBox
-
     def refill_combo_from_dict(self, combo_box, dictionary):
+        """
+        Refill a combo box with items from a dictionary.
+
+        Args:
+            combo_box (QComboBox): The combo box to be refilled.
+            dictionary (dict): The dictionary containing items to add to the combo box.
+        """
         combo_box.clear()  # Clear the combo box
         combo_box.addItem("Select a Signal")
+
         # Add items from the dictionary to the combo box
         if self.way_of_plotting_with_add:
             for key in dictionary:
@@ -194,6 +298,12 @@ class MainApp(QMainWindow, FORM_CLASS):  # go to the main window in the form_cla
                 combo_box.addItem(key)
 
     def delete_signal(self):
+        """
+        Delete a selected signal and update the UI.
+
+        Depending on the plotting mode (add or replace), the function deletes the selected signal
+        and updates the combo box and signal data accordingly.
+        """
         if self.way_of_plotting_with_add:
             selected_item_text = self.comboBox_2.currentText()
             selected_signal_index = int(selected_item_text.split('-')[-1].strip())
--- mixer.py
+++ mixer.py
@@ -1,48 +1,81 @@
+# Import necessary libraries and modules
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.uic import loadUiType
-from UliEngineering.SignalProcessing.Simulation import sine_wave
-import os
+from UliEngineering.SignalProcessing.Simulation import sine_wave  # Import custom signal processing module
 import sys
 from os import path
 import numpy.fft as fft
 import numpy as np
 import pyqtgraph as pg
-from main import MainApp
-FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "mixer.ui"))  # connects the Ui file with the Python file
+from main import MainApp  # Assuming there's a 'main' module for the main application
 
+# Load the UI file for the mixer application
+FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "mixer.ui"))
 
+# Define the MixerApp class, a QDialog-based application for constructing and visualizing synthetic signals
 class MixerApp(QDialog, FORM_CLASS):
-
     def __init__(self, parent=None):
-        super(MixerApp, self).__init__(parent)
+        """
+        Initialize the MixerApp.
 
+        :param parent: The parent widget.
+        """
+        super(MixerApp, self).__init__(parent)
+        # Initialize the UI
         self.setupUi(self)
+        # Set up event handlers for buttons
         self.handle_btn()
+        # Initialize time array for sine wave simulation
         self.sin_time = np.linspace(0, 3, 4000)
-        self.sinusoidals = {}  # Store sine waves as a dictionary
+        # Store sine waves as a dictionary
+        self.sinusoidals = {}
         self.sin_names = []
+        # Set the background color of the graphics view
         self.sin_graphics_view.setBackground('w')
+        # Set the x-axis label of the graphics view 
+        self.sin_graphics_view.setLabel('bottom', 'time')
 
     def handle_btn(self):
+        """
+        Connect buttons to their respective functions.
+        """
         self.plot_push_btn.clicked.connect(self.construct_signal)
         self.add_push_btn.clicked.connect(self.add_to_main)
 
     def add_to_main(self):
+        """
+        Add synthesized signals to the main application and update UI elements.
+        """
+        # Clear all existing signals in the main application
         self.mainapp.clear_all()
+
+        # Populate a combo box in the main application with synthesized signals
         self.mainapp.refill_combo_from_dict(self.mainapp.comboBox_2, self.sinusoidals)
+
+        # Calculate the sample rate and set frequency slider values
         self.mainapp.fs = 2 * self.overall_max_frequency
-        self.mainapp.freq_slider.setRange(int(self.mainapp.fs / 2), int(self.mainapp.fs * 3 / 2))
+        self.mainapp.freq_slider.setRange(int(self.mainapp.fs / 4), int(self.mainapp.fs * 4))
         self.mainapp.freq_slider.setSliderPosition(int(2 * self.overall_max_frequency))
-        self.mainapp.freq_slider.setValue(int(self.mainapp.fs))  # Set the value to 125
+        self.mainapp.freq_slider.setValue(int(self.mainapp.fs))
+
+        # Plot the synthetic signal in the main application
         self.mainapp.plot_graph()
 
     def set_myapp(self, mainapp):
+        """
+        Set the main application.
+
+        :param mainapp: The main application instance.
+        """
         self.mainapp = mainapp
 
     def construct_signal(self):
+        """
+        Create a synthetic signal based on user inputs.
+        """
         # Get input values from QLineEdit widgets
         freq_input = self.signalFrequency.text()
         magnitude_input = self.signalMagnitude.text()
@@ -83,9 +116,14 @@ class MixerApp(QDialog, FORM_CLASS):
                                     phaseshift=sin_phase)
         self.sinusoidals[name_input] = self.sinusoidal
         self.sin_names.append(name_input)
+
+        # Draw and display the synthetic signal
         self.drawSyntheticSignal(self.sin_graphics_view)
 
     def sumSignals(self):
+        """
+        Calculate the synthetic signal by summing individual sine wave components.
+        """
         if self.sinusoidals:
             self.syntheticSignal = np.zeros(len(self.sin_time))  # Initialize with zeros
             for name, sinusoidal in self.sinusoidals.items():
@@ -94,15 +132,31 @@ class MixerApp(QDialog, FORM_CLASS):
             self.syntheticSignal = None
 
     def drawSyntheticSignal(self, graphicsView):
+        """
+        Draw and display the synthetic signal on a graphics view.
+
+        :param graphicsView: The graphics view to display the signal.
+        """
         self.sumSignals()
         graphicsView.clear()
+
+        # Plot the synthetic signal
         graphicsView.plot(self.sin_time, self.syntheticSignal, pen=pg.mkPen(color=(255, 0, 0)))
+
+        # Calculate sample rate and maximum frequencies
         self.sample_rate = len(self.sin_time)
         self.max_freqs = self.calculate_max_frequencies(list(self.sinusoidals.values()), self.sample_rate)
         self.overall_max_frequency = max(self.max_freqs)
 
     def calculate_max_frequency(self, sinusoidal, sample_rate):
-    # Step 1: Compute the Fourier Transform of the individual sinusoidal component.
+        """
+        Calculate the maximum frequency of a sine wave component using FFT.
+
+        :param sinusoidal: The input sine wave.
+        :param sample_rate: The sample rate.
+        :return: The maximum frequency.
+        """
+        # Step 1: Compute the Fourier Transform of the individual sinusoidal component.
         fft_result = fft.fft(sinusoidal)
 
         # Step 2: Find the frequency components in the FFT result.
@@ -124,6 +178,13 @@ class MixerApp(QDialog, FORM_CLASS):
         return max_frequency
 
     def calculate_max_frequencies(self, sinusoidals, sample_rate):
+        """
+        Calculate the maximum frequencies for all sine wave components.
+
+        :param sinusoidals: A list of sine wave components.
+        :param sample_rate: The sample rate.
+        :return: A list of maximum frequencies.
+        """
         max_frequencies = []
         for sinusoidal in sinusoidals:
             max_freq = self.calculate_max_frequency(sinusoidal, sample_rate)
